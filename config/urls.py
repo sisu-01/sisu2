a2cbe9e3f2ffc18a1be80c464c31ce6b57ef5dc4
sisu-01 <tesv010615@naver.com>
2023-08-25 14:24:52 +0900

    movie app 추가

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('common.urls')),
     path('blog/', include('blog.urls')),
+    path('movie/', include('movie.urls')),
 ]
