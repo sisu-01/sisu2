349b042e9663df1677b3e667fb0cc9395c5cb9fa
sisu-01 <tesv010615@naver.com>
2023-09-08 11:29:36 +0900

    movie 썸네일 크기 조정

--- movie/forms.py
+++ movie/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from io import BytesIO
-from PIL import Image as pil
+from PIL import Image
 from .models import *
 
 class MovieForm(forms.ModelForm):
@@ -21,47 +21,35 @@ class MovieForm(forms.ModelForm):
 
     def save(self, commit=True):
         instance = super(MovieForm, self).save(commit=False)
-
         #init에서 FILES가 담겼는가?
         if(self.poster):
             import copy
             thumbnail = copy.copy(self.poster)
-            #이제 self.poster 을 조리해서 instance.thumbnail 에 넣어주면 컄
-            temp = self.rescale(thumbnail, 100, 300, force=True)
+            temp = self.rescale(thumbnail, 240, 385)
             instance.thumbnail = temp
         if commit:
             instance.save()
 
         return instance
     
-    def rescale(self, data, width, height, force=True):
-        max_width = width
-        max_height = height
-
+    def rescale(self, data, width, height):
         input_file = BytesIO(data.read())
-        img = pil.open(input_file)
-        if not force:
-            img.thumbnail((max_width, max_height), pil.LANCZOS)
+        img = Image.open(input_file)
+        img_width, img_height = img.size
+        img_ratio = float(img_width)/float(img_height)
+        ratio_w = float(width)/float(height)
+        ratio_h = float(height)/float(width)
+        if img_ratio < ratio_w:
+            blank = int(img_height-(img_width/ratio_w))
+            img_height = int(img_width/ratio_w)
+            (left, upper, right, lower) = (0, blank//2, img_width, blank//2+img_height)
         else:
-            src_width, src_height = img.size
-            src_ratio = float(src_width) / float(src_height)
-            dst_width, dst_height = max_width, max_height
-            dst_ratio = float(dst_width) / float(dst_height)
-
-            if dst_ratio < src_ratio:
-                crop_height = src_height
-                crop_width = crop_height * dst_ratio
-                x_offset = int(src_width - crop_width) // 2
-                y_offset = 0
-            else:
-                crop_width = src_width
-                crop_height = crop_width / dst_ratio
-                x_offset = 0
-                y_offset = int(src_height - crop_height) // 3
-            img = img.crop((x_offset, y_offset, x_offset+int(crop_width), y_offset+int(crop_height)))
-            img = img.resize((dst_width, dst_height), pil.LANCZOS)
-
+            blank = int(img_width-(img_height/ratio_h))
+            img_width = int(img_height/ratio_h)
+            (left, upper, right, lower) = (blank//2, 0, blank//2+img_width, img_height)
+        img = img.crop((left, upper, right, lower))
+        img = img.resize((width,height), Image.LANCZOS)
         image_file = BytesIO()
-        img.save(image_file, 'png')
+        img.save(image_file, format='JPEG')
         data.file = image_file
         return data
