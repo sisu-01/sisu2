fb588b97d311c605cb5ff7b32f04e73cf02eac90
sisu-01 <tesv010615@naver.com>
2023-09-08 14:01:33 +0900

    최상위 메뉴 모델 추가

--- common/models.py
+++ common/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class TopMenu(models.Model):
+    url = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=20)
+    order = models.IntegerField()
+
+    class Meta:
+        managed = True
+        db_table = 't_top_menu'
+        db_table_comment = '최상위 메뉴'
+
+    def __str__(self):
+            return self.name
