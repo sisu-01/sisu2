287e66780d825866030cb11ed93fc32bb2cc33b3
sisu-01 <tesv010615@naver.com>
2023-09-22 10:01:24 +0900

    blog tree menu 필터화

--- blog/views.py
+++ blog/views.py
@@ -11,47 +11,20 @@ from datetime import datetime
 
 # Create your views here.
 def index(request):
-    temp = Menu.objects.all().order_by('seq')
-    result = get_menu_tree(temp)
     context = {
-        'menus': result
     }
     return render(request, 'blog/blog.html', context)
 
-def get_menu_tree(menus, parent=None):
-    tree = []
-    for menu in menus:
-        if menu.id_parent_id == parent:
-            child = get_menu_tree(menus, menu.id)
-            if child:  
-                tree.append({
-                    'id': menu.id,
-                    'title': menu.title,
-                    'child': child,
-                })
-            else:
-                tree.append({
-                    'id': menu.id,
-                    'title': menu.title,
-                })
-    return tree
-
 def list(request, id):
-    temp = Menu.objects.all().order_by('seq')
-    result = get_menu_tree(temp)
     postList = Post.objects.filter(menu=id).values('id','title')
     context = {
-        'menus': result,
         'postList': postList
     }
     return render(request, 'blog/blog_list.html', context)
 
 def post(request, id):
-    temp = Menu.objects.all().order_by('seq')
-    result = get_menu_tree(temp)
     post = Post.objects.get(id=id)
     context = {
-        'menus': result,
         'post': post,
     }
     return render(request, 'blog/blog_post.html', context)
