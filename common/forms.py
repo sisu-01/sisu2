e349b4661ad0c568f76b6ab15a18483c6902e841
sisu-01 <tesv010615@naver.com>
2023-10-30 15:44:36 +0900

    프로필 form

--- common/forms.py
+++ common/forms.py
@@ -1,5 +1,11 @@
 from django.contrib.auth import forms as auth_forms
 from django import forms
+from .models import Profile
 
 class LoginForm(auth_forms.AuthenticationForm):
     remember = forms.BooleanField(required=False)
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['nickname','desc','image']
