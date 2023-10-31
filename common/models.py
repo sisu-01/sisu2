6011f6d0b810aaae2cce0f3463c541006884bd2c
sisu-01 <tesv010615@naver.com>
2023-10-31 13:29:50 +0900

    TopMenu 하드코딩

--- common/models.py
+++ common/models.py
@@ -1,18 +1,5 @@
 from django.db import models
 from common.utils import ImageUpload
-
-class TopMenu(models.Model):
-    url = models.CharField(primary_key=True, max_length=20)
-    name = models.CharField(max_length=20)
-    order = models.IntegerField()
-
-    class Meta:
-        managed = True
-        db_table = 't_top_menu'
-        db_table_comment = '최상위 메뉴'
-
-    def __str__(self):
-            return self.name
     
 class Profile(models.Model):
     nickname = models.CharField(max_length=10)
--- common/templatetags/filter.py
+++ common/templatetags/filter.py
@@ -1,13 +1,7 @@
 from django import template
-from common.models import TopMenu
 
 register = template.Library()
 
 @register.filter
 def weekday(value):
     return ['월','화','수','목','금','토','일'][value]
-
-@register.simple_tag
-def top_menu():
-    menu = TopMenu.objects.all().order_by('order')
-    return menu
