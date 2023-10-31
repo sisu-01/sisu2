36ca23c02ab76ab84e9ff25280c7a8a4f6ac4e32
sisu-01 <tesv010615@naver.com>
2023-10-31 14:45:42 +0900

    admin url 주석

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', include('common.urls')),
     path('blog/', include('blog.urls')),
     path('movie/', include('movie.urls')),
